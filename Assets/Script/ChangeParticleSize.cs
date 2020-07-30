fe7e3574aeb39166842eccc39ccf3f71a603ff4f
yv4 <2423765460@qq.com>
2020-07-30 19:41:44 +0800

    特效修改

---
+++ Assets/Script/ChangeParticleSize.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ChangeParticleSize : MonoBehaviour
+{
+    public int Size = 3;
+
+    private void Awake()
+    {
+        ParticleSystem particle = BaseOption.FindChild(this.gameObject, "tiaodai").GetComponent<ParticleSystem>();
+        var main = particle.main;
+        main.startSize = new ParticleSystem.MinMaxCurve(Size);
+        particle.gameObject.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Undo.RecordObject(this.gameObject, "Modify Obj");
+        EditorUtility.SetDirty(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
